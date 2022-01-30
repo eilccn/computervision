a58e7af6af227cace9aa75e909557c4fe22ae20c
Eileen Chang <chng.eiln@gmail.com>
2022-01-30 16:06:13 -0500

    edit 7

--- project1/part3/vidDisplay.cpp
+++ project1/part3/vidDisplay.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[]) {
 	char label[256];
 	int quit = 0;
 	int frameid = 0;
+	int grayid = 0;
 	char buffer[256];
 	std::vector<int> pars;
 
@@ -30,10 +31,10 @@ int main(int argc, char *argv[]) {
 
 	printf("Expected size: %d %d\n", refS.width, refS.height);
 
-	cv::namedWindow("Video", 1); // identifies a window?
 	cv::Mat frame;
+	cv::namedWindow("Video", cv::WINDOW_AUTOSIZE); 
 
-	
+
 	for(;!quit;) {
 		*capdev >> frame; // get a new frame from the camera, treat as a stream
 
@@ -41,9 +42,9 @@ int main(int argc, char *argv[]) {
 		  printf("frame is empty\n");
 		  break;
 		}
-		
+			
 		cv::imshow("Video", frame);
-		
+	
 		// see if there is a waiting keystroke
 		int key = cv::waitKey(10);
 
@@ -52,26 +53,30 @@ int main(int argc, char *argv[]) {
 		    quit = 1;
 		    break;
 
-		    
 		case 's': // capture a photo if the user hits s
 		    sprintf(buffer, "%s.%03d.png", label, frameid++);
 		    cv::imwrite(buffer, frame, pars);
 		    printf("Image written: %s\n", buffer);
 		    break;
-		
-		case 'g': // display greyscale live video
-		    cv::Mat grayscale;
-		    cvtColor(frame, grayscale, cv::COLOR_BGR2GRAY);  
-		    cv::imshow("Video", grayscale);
-		        
- 
-		    if (waitKey(30) == 27)
-		    {
-		    	break;
-		    }
-		    
-		}
 
+		case 'g': // display greyscale live video
+                    cv::Mat gray_image;
+                    cv::namedWindow("Grayscale", WINDOW_AUTOSIZE);
+                    cv::cvtColor(frame, gray_image, COLOR_BGR2GRAY);
+                    cv::imshow("Grayscale", gray_image);
+
+		    int key = waitKey(10);
+		    if (key == 's')
+		    {
+		        sprintf(buffer, "%s.%03d.png", label, grayid++);
+                        cv::imwrite(buffer, gray_image, pars);
+                        printf("Image written: %s\n", buffer);
+                        break;
+    
+		    }
+		    break;
+		
+		}
 
 	}
 
