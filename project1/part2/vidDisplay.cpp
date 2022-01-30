475fe330449a387bf1ec7b78cbdaf8d61022ed45
Eileen Chang <chng.eiln@gmail.com>
2022-01-29 22:02:33 -0500

    edit 5

--- project1/part2/vidDisplay.cpp
+++ project1/part2/vidDisplay.cpp
@@ -1,21 +1,69 @@
 #include <cstdio>
 #include <opencv2/opencv.hpp>
 
-int main()
-{
-	cv::VideoCapture video(0);
 
-	if(!video.isOpened()) return -1;
+int main(int argc, char *argv[]) {
+	char label[256];
+	int quit = 0;
+	int frameid = 0;
+	char buffer[256];
+	std::vector<int> pars;
 
+	pars.push_back(5);
+
+	cv::VideoCapture *capdev;
+
+	// open the video device
+	capdev = new cv::VideoCapture(0);
+	if( !capdev->isOpened() ) {
+		printf("Unable to open video device\n");
+		return(-1);
+	}
+
+	// get some properties of the image
+	cv::Size refS( (int) capdev->get(cv::CAP_PROP_FRAME_WIDTH ),
+		       (int) capdev->get(cv::CAP_PROP_FRAME_HEIGHT));
+
+	printf("Expected size: %d %d\n", refS.width, refS.height);
+
+	cv::namedWindow("Video", 1); // identifies a window?
 	cv::Mat frame;
 
-	while(video.read(frame))
-	{
-		cv::imshow("Video feed", frame);
+	
+	for(;!quit;) {
+		*capdev >> frame; // get a new frame from the camera, treat as a stream
 
-		if (cv::waitKey(25) >= 0)
-		{
-			break;
+		if( frame.empty() ) {
+		  printf("frame is empty\n");
+		  break;
 		}
+		
+		cv::imshow("Video", frame);
+		
+		// see if there is a waiting keystroke
+		int key = cv::waitKey(10);
+
+		switch(key) {
+		case 'q':
+		    quit = 1;
+		    break;
+
+		    
+		case 's': // capture a photo if the user hits s
+		    sprintf(buffer, "%s.%03d.png", label, frameid++);
+		    cv::imwrite(buffer, frame, pars);
+		    printf("Image written: %s\n", buffer);
+		    break;
+
+		default:
+		    break;
+		}
+
 	}
+
+	// terminate the video capture
+	printf("Terminating\n");
+	delete capdev;
+
+	return(0);
 }
