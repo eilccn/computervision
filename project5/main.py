be4340147ba6958b03361cf51670ec1d7e988dc4
Eileen Chang <chng.eiln@gmail.com>
2022-04-19 21:17:14 -0400

    main

--- project5/main.py
+++ project5/main.py
@@ -1,15 +1,18 @@
 # Eileen Chang
 # Project 5: Recognition Using Deep Networks
 
-# import statements
+# IMPORT STATEMENTS
 import sys
 import numpy
 import random
 import torch
 import torchvision
 import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
-# prepare dataset
+# PREPARE DATASET
 n_epochs = 3
 batch_size_train = 64
 batch_size_test = 1000
@@ -21,7 +24,7 @@ random_seed = 1
 torch.backends.cudnn.enabled = False
 torch.manual_seed(random_seed)
 
-# data loader
+# DATA LOADER
 train_loader = torch.utils.data.DataLoader(
   torchvision.datasets.MNIST('/Users/eileenchang/computervision/project5/images', train=True, download=True,
                              transform=torchvision.transforms.Compose([
@@ -40,21 +43,43 @@ test_loader = torch.utils.data.DataLoader(
                              ])),
   batch_size=batch_size_test, shuffle=True)
 
-# example
-examples = enumerate(test_loader)
-batch_idx, (example_data, example_targets) = next(examples)
 
-example_data.shape
-torch.Size([1000, 1, 28, 28])
+# FUNCTIONS 
 
-# plot first 6 example digits
-fig = plt.figure()
-for i in range(6):
-  plt.subplot(2,3,i+1)
-  plt.tight_layout()
-  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-  plt.title("Ground Truth: {}".format(example_targets[i]))
-  plt.xticks([])
-  plt.yticks([])
-fig
-plt.show()
+# first 6 example digits
+def example():
+  examples = enumerate(test_loader)
+  batch_idx, (example_data, example_targets) = next(examples)
+
+  example_data.shape
+  torch.Size([1000, 1, 28, 28])
+
+  # plot first 6 example digits
+  fig = plt.figure()
+  for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.tight_layout()
+    plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+    plt.title("Ground Truth: {}".format(example_targets[i]))
+    plt.xticks([])
+    plt.yticks([])
+  fig
+  plt.show()
+
+
+# MAIN FUNCTION
+def main(argv):
+    # handle any command line arguments in argv
+
+    # main function code
+
+    # in order to make network repeatable set the random seed for the torch package
+    torch.manual_seed(42) # remove this line if you want to create different networks
+    torch.backends.cudnn.enabled = False # turn off CUDA
+
+    example()
+
+    return
+
+if __name__ == "__main__":
+    main(sys.argv)
