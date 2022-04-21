1bd7734ffc73f142248803f310f0b8f20d9078e2
Eileen Chang <chng.eiln@gmail.com>
2022-04-21 17:06:39 -0400

    read network

--- project5/task1f.py
+++ project5/task1f.py
@@ -11,7 +11,7 @@ with torch.no_grad():
 
 fig = plt.figure()
 for i in range(9):
-  plt.subplot(2,3,i+1)
+  plt.subplot(3,3,i+1)
   plt.tight_layout()
   plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
   plt.title("Prediction: {}".format(
@@ -19,5 +19,6 @@ for i in range(9):
   plt.xticks([])
   plt.yticks([])
 fig
+plt.show()
 
 
