da0b1b2a390a13e2c7d23d25367d99f89fa761ac
Eileen Chang <chng.eiln@gmail.com>
2022-04-21 16:55:40 -0400

    read network

--- project5/main.py
+++ project5/main.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 # PREPARE DATASET
-n_epochs = 3
+n_epochs = 5
 batch_size_train = 64
 batch_size_test = 1000
 learning_rate = 0.01
@@ -43,6 +43,24 @@ test_loader = torch.utils.data.DataLoader(
                              ])),
   batch_size=batch_size_test, shuffle=True)
 
+# CLASS DEFINITIONS
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
 
 # FUNCTIONS 
 
@@ -66,6 +84,63 @@ def example():
   fig
   plt.show()
 
+# initialize network and optimizer
+network = Net()
+optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+                      momentum=momentum)
+
+# train the model
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+def train(epoch):
+  network.train()
+  for batch_idx, (data, target) in enumerate(train_loader):
+    optimizer.zero_grad()
+    output = network(data)
+    loss = F.nll_loss(output, target)
+    loss.backward()
+    optimizer.step()
+    if batch_idx % log_interval == 0:
+      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        epoch, batch_idx * len(data), len(train_loader.dataset),
+        100. * batch_idx / len(train_loader), loss.item()))
+      train_losses.append(loss.item())
+      train_counter.append(
+        (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+      torch.save(network.state_dict(), '/Users/eileenchang/computervision/project5/results/model.pth')
+      torch.save(optimizer.state_dict(), '/Users/eileenchang/computervision/project5/results/optimizer.pth')
+
+# model training test loop
+def test():
+  network.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in test_loader:
+      output = network(data)
+      test_loss += F.nll_loss(output, target, size_average=False).item()
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+  test_loss /= len(test_loader.dataset)
+  test_losses.append(test_loss)
+  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
+
+# plot training curve
+def training_curve():
+  fig = plt.figure()
+  plt.plot(train_counter, train_losses, color='blue')
+  plt.scatter(test_counter, test_losses, color='red')
+  plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+  plt.xlabel('number of training examples seen')
+  plt.ylabel('negative log likelihood loss')
+  plt.show()
+  fig
+
 
 # MAIN FUNCTION
 def main(argv):
@@ -77,7 +152,20 @@ def main(argv):
     torch.manual_seed(42) # remove this line if you want to create different networks
     torch.backends.cudnn.enabled = False # turn off CUDA
 
+    # plot first 6 example digits
     example()
+
+    # training model test loop
+    test()
+    for epoch in range(1, n_epochs + 1):
+      train(epoch)
+      test()
+
+    # plot training curve
+    training_curve()
+
+  
+
 
     return
 
---
+++ project5/task1f.py
@@ -0,0 +1,12 @@
+# Eileen Chang
+# Project 5: Recognition Using Deep Networks
+# Task 1F - Read network and run it on test set
+
+# IMPORT STATEMENTS
+from main import *
+
+
+with torch.no_grad():
+    output = network(example_data)
+
+
