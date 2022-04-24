731ea957e1bbc06f729f02e34e0af21c65611482
Eileen Chang <chng.eiln@gmail.com>
2022-04-24 04:06:05 -0400

    1g

--- project5/task1g.py
+++ project5/task1g.py
@@ -10,9 +10,6 @@ import torch
 import numpy as np
 from torchvision import transforms, datasets
 
-# CLASS DEFINITIONS
-
-
 # initialize new set of network and optimizers
 continued_network2 = Net()
 continued_optimizer2 = optim.SGD(network.parameters(), lr=learning_rate,
@@ -25,35 +22,49 @@ continued_network2.load_state_dict(network_state_dict)
 optimizer_state_dict = torch.load("/Users/eileenchang/computervision/project5/results/optimizer.pth")
 continued_optimizer2.load_state_dict(optimizer_state_dict)
 
-data_transform = transforms.Compose([
-        transforms.Resize([28,28]),
-        transforms.Grayscale(num_output_channels=1),
-        transforms.transforms.ToTensor(),
-        transforms.ConvertImageDtype(torch.float),
-        transforms.Normalize([0.485], [0.229])
-        ])
+# FUNCTION DEFINITIONS
+# test network on new input 
+def handwritten_test(root_path):
+    data_transform = transforms.Compose([
+            transforms.Resize([28,28]),
+            transforms.Grayscale(num_output_channels=1),
+            transforms.transforms.ToTensor(),
+            transforms.ConvertImageDtype(torch.float),
+            transforms.Normalize([0.485], [0.229])
+            ])
 
-my_dataset = datasets.ImageFolder(root="./root/",
-        transform=data_transform)
+    my_dataset = datasets.ImageFolder(root=root_path,
+            transform=data_transform)
 
-dataset_loader = torch.utils.data.DataLoader(my_dataset,
-        batch_size=batch_size_test, shuffle=True, num_workers=0)
+    dataset_loader = torch.utils.data.DataLoader(my_dataset,
+            batch_size=batch_size_test, shuffle=True, num_workers=0)
 
-samples = enumerate(dataset_loader)
-batch_xyz, (sample_data, sample_targets) = next(samples)
+    samples = enumerate(dataset_loader)
+    batch_xyz, (sample_data, sample_targets) = next(samples)
 
-with torch.no_grad():
-  my_output = continued_network2(sample_data)
+    # run the handwritten digit data against the pre-trained network
+    with torch.no_grad():
+        my_output = continued_network2(sample_data)
+
+    # plot the predictions
+    fig = plt.figure()
+    for i in range(10):
+        plt.subplot(4,3,i+1)
+        plt.tight_layout()
+        plt.imshow(sample_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Prediction: {}".format(
+        my_output.data.max(1, keepdim=True)[1][i].item()))
+        plt.xticks([])
+        plt.yticks([])
+    fig
+    plt.show()
 
 
-fig = plt.figure()
-for i in range(9):
-    plt.subplot(3,3,i+1)
-    plt.tight_layout()
-    plt.imshow(sample_data[i][0], cmap='gray', interpolation='none')
-    plt.title("Prediction: {}".format(
-    my_output.data.max(1, keepdim=True)[1][i].item()))
-    plt.xticks([])
-    plt.yticks([])
-fig
-plt.show()
+# MAIN FUNCTION
+def main(argv):
+    handwritten_test("./root/")
+    
+    return
+
+if __name__ == "__main__":
+    main(sys.argv)
